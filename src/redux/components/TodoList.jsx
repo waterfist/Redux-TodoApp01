554e62aeeeeae7cb1678a44169b64352eec97e88
hotsix <codinggo@naver.com>
2023-01-04 01:32:05 +0900

    (6) TodoList에서 삭제 구현

--- src/redux/components/TodoList.jsx
+++ src/redux/components/TodoList.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
+import { removeTodo } from "../modules/todos";
 
 function TodoList({ isActive }) {
   // store에 있는 todos를 가지고 온 것.
   // state는 상태를 모아 놓은 것. state 안에 있는 todos를 가지고 온다.
   const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  const handleDeleteButtonClick = (id) => {
+    dispatch(removeTodo(id));
+  };
   return (
     <StyledListBox>
       <h4>{isActive ? "해야할 일" : "완료된 일"}</h4>
@@ -13,11 +18,13 @@ function TodoList({ isActive }) {
         .filter((item) => item.isDone === !isActive)
         .map((item) => {
           return (
-            <StyledTodoBox>
+            <StyledTodoBox key={item.id}>
               <h4>{item.title}</h4>
               <p>{item.contents}</p>
               <button>완료</button>
-              <button>삭제</button>
+              <button onClick={() => handleDeleteButtonClick(item.id)}>
+                삭제
+              </button>
             </StyledTodoBox>
           );
         })}
